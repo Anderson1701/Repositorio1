bf22359d3bd7e249dd320651f15b709dbc122a9a
Anderson Soto <agsoto1@espe.edu.ec>
2024-11-09 15:32:44 -0500

    subiendo el proyecto

---
+++ DatosPrimitivos/src/datosprimitivos/DatosPrimitivos.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datosprimitivos;
+
+/**
+ *
+ * @author ander
+ */
+public class DatosPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b=12;
+        short s=4121;
+        int i=1542245;
+        long l=12498554L;
+        
+        float f=12.21547F;
+        double d=42.1587469854126;
+        
+        char c='S';
+        
+        boolean v=false;
+        
+        System.out.println("Dato tipo byte:"+b);
+        System.out.println("Dato tipo short:"+s);
+        System.out.println("Dato tipo int"+i);
+        System.out.println("Dato tipo long"+l);
+        System.out.println("Dato tipo float:"+f);
+        System.out.println("Dato tipo double"+d);
+        System.out.println("Dato tipo char:"+c);
+        System.out.println("Dato tipo boolean"+v);
+    }
+    
+}
